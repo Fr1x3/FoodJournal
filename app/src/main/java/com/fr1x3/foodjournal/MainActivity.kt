eafc70db225328b89255e03147a3dbfee95ba2fd
Fr1x3 <gathufx@gmail.com>
2021-09-20 14:03:46 -0700

    set up databinding

--- app/src/main/java/com/fr1x3/foodjournal/MainActivity.kt
+++ app/src/main/java/com/fr1x3/foodjournal/MainActivity.kt
@@ -2,11 +2,13 @@ package com.fr1x3.foodjournal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.fr1x3.foodjournal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main )
     }
 
 
